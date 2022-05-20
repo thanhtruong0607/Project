b4dc249318eb230b822ee722758492079a5339a4
Lam Long <longlam@hhdgroup.com>
2022-05-20 15:00:21 +0700

    support call api + training

--- TestProject/client/src/App.js
+++ TestProject/client/src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
-import HomeScreen from './views/HomeScreen';
-import LoginForm from './components/LoginForm';
+import React from "react";
+import logo from "./logo.svg";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import HomeScreen from "./views/HomeScreen";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route exact path='/' element={<HomeScreen />} />
-          <Route path='/login' element={<LoginForm />} />
+          <Route exact path="/" element={<HomeScreen />} />
+          <Route path="/login" element={<LoginForm />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
