be5212eef49175afa257cecafb7ef01aab6d32c1
Nguyen Thanh Truong <truong111120@gmail.com>
2022-06-03 22:24:32 +0700

    commit

--- TestProject/client/src/service/axios.js
+++ TestProject/client/src/service/axios.js
@@ -2,46 +2,47 @@ import axios from "axios";
 import { HOST, USER_KEY } from "../utils/config";
 
 class Service {
-  post = async (params, sub_url = HOST.COMMON) => {
-    try {
-      let url = `${HOST.MAIN}${sub_url}`;
-      let result = await axios.post(
-        url,
-        { params },
-        { headers: { HTTP_API_KEY: USER_KEY.KEY } }
-      );
 
-      if (result.status === 200) {
-        if (result.data.error) {
-          return {
-            status: "FAILED",
-            data: result.data.error,
-          };
-        } else {
-          return {
-            status: "SUCCESS",
-            data: result.data,
-          };
+    post = async (params, sub_url = HOST.COMMON) => {
+        try {
+            let url = `${HOST.MAIN}${sub_url}`;
+            let result = await axios.post(
+                url,
+                { params },
+                { headers: { HTTP_API_KEY: USER_KEY.KEY } }
+            );
+
+            if (result.status === 200) {
+                if (result.data.error) {
+                    return {
+                        status: "FAILED",
+                        data: result.data.error,
+                    };
+                } else {
+                    return {
+                        status: "SUCCESS",
+                        data: result.data,
+                    };
+                }
+            } else {
+                console.log(`Error: `, result);
+                return {
+                    status: "FAILED",
+                    data: {
+                        message: "Error when call API",
+                    },
+                };
+            }
+        } catch (error) {
+            console.log(`Error: `, error);
+            return {
+                status: "FAILED",
+                data: {
+                    message: "Error when call API",
+                },
+            };
         }
-      } else {
-        console.log(`Error: `, result);
-        return {
-          status: "FAILED",
-          data: {
-            message: "Error when call API",
-          },
-        };
-      }
-    } catch (error) {
-      console.log(`Error: `, error);
-      return {
-        status: "FAILED",
-        data: {
-          message: "Error when call API",
-        },
-      };
-    }
-  };
+    };
 }
 
 const service = new Service();
--- TestProject/index.js
+++ TestProject/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 require('./db/config');
 const User = require('./db/User')
 
-const cors = require('cors');
+// const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+// app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Server is working')
